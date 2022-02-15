128bdbd9e8e31bed0cb9e678e2f933238b65dd7d
Natawat Hebnak <minin0la17@gmail.com>
2022-02-15 18:34:16 +0800

    from netlify

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ handleDisconnect();
 app.set("view engine", "ejs");
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://pdm-ecrp.netlify.app"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
