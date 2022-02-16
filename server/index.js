601550c62bb0c1606c0f6fd1a60800a057c5ed7a
Natawat Hebnak <minin0la17@gmail.com>
2022-02-16 12:42:34 +0800

    Change to allow anywhere for testing

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ handleDisconnect();
 app.set("view engine", "ejs");
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://pdm-ecrp.netlify.app"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   // res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
